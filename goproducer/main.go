85b4e36b4eb08e8c95b01b3f4e73563ae1e86093
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2020-03-26 17:52:12 -0300

    feat: create producer

---
+++ goproducer/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"go-consumer-producer-poc/amqp"
+	"go-consumer-producer-poc/structs"
+	"log"
+)
+
+const queueName = "queue-test"
+
+func generateMessage(str string) []byte {
+	message := structs.QueueMessage{
+		Title:     str,
+		Timestamp: 0,
+	}
+	body, err := json.Marshal(message)
+	fatalError(err, "JSONMarshlingException")
+	return body
+}
+
+func print(str string) {
+	fmt.Printf(str + "\n")
+}
+
+func printErrorIfExists(err error) {
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+}
+
+func fatalError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("[%s]: %s", msg, err)
+	}
+}
+
+func main() {
+	print("Go Producer")
+	print("Trying to connect to RabbitMQ...")
+	connection, err := amqp.ConnectToAMQP()
+	fatalError(err, "ConnectionException")
+	print("Trying to assert queue...")
+	channel, routingKey, err := amqp.ConnectToChannelAndAssertQueue(connection, queueName)
+	fatalError(err, "ChannelAndQueueException")
+	message := generateMessage("Oisss")
+	print("Sending message...")
+	err = amqp.PublishMessage(channel, routingKey, message)
+	printErrorIfExists(err)
+}
